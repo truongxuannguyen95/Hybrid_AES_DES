5f7557f00200510a5e7e857d26172331d144cd97
Nguyên Trương <spyforlove2015@gmail.com>
2018-11-11 23:47:25 +0700

    Trương Xuân Nguyên 23:45 11/11/2018

---
+++ app/src/main/java/com/example/nguyen/hybrid_aes_des/activity/Intro.java
@@ -0,0 +1,35 @@
+package com.example.nguyen.hybrid_aes_des.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.nguyen.hybrid_aes_des.R;
+
+public class Intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro);
+        finishIntro();
+    }
+
+    public void finishIntro() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(Intro.this, UserPage.class));
+                finish();
+            }
+        }).start();
+    }
+}
